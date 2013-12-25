14c1babe9796fa66892d466a61607c491378d0ab
Mike Fitterman <mfitterman@epsilon.com>
2013-12-25 13:25:08 -0500

    Make a basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131224172222) do
+ActiveRecord::Schema.define(version: 20131225180833) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
