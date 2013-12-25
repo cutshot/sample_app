eebe3c3820ba18ec43643c386516de1751d95fc6
Mike Fitterman <mfitterman@epsilon.com>
2013-12-25 18:23:44 -0500

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
-  attr_accessor :name, :email
+ 	def new
+ 		@user = User.new
+  	end
 
-  def new
+	def show
+    	@user = User.find(params[:id])
+  	end
+
+ def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
